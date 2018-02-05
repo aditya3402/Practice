5aeb5d2e6bae5b4c1f4a659e34892c2588854e24
ajena <aditya.ece.iitkgp@gmail.com>
2018-02-05 22:00:42 +0530

    Adding DFS Traversals

--- Eclipse/Prep/src/Utils/BTree/BinaryTree.java
+++ Eclipse/Prep/src/Utils/BTree/BinaryTree.java
@@ -27,8 +27,48 @@ public class BinaryTree<T>
 		return root;
 	}
 	
-	public Inorder()
+	public void Inorder()
 	{
-		
+		Inorder(root);
+	}
+	
+	public void Preorder()
+	{
+		Preorder(root);
+	}
+	
+	public void Postorder()
+	{
+		Postorder(root);
+	}
+	
+	private void Inorder(Node<T> root)
+	{
+		if(root != null)
+		{
+			Inorder(root.left);
+			System.out.print("\t" + root.data);
+			Inorder(root.right);
+		}
+	}
+	
+	private void Preorder(Node<T> root)
+	{
+		if(root != null)
+		{
+			System.out.print("\t" + root.data);
+			Preorder(root.left);
+			Preorder(root.right);
+		}
+	}
+	
+	private void Postorder(Node<T> root)
+	{
+		if(root != null)
+		{
+			Postorder(root.left);			
+			Postorder(root.right);
+			System.out.print("\t" + root.data);
+		}
 	}
 }
